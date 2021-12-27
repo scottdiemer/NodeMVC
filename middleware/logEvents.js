38ff43a694c14770e2c374b9679ede3d7cd38d78
scottdiemer <sdiemer@cfl.rr.com>
2021-12-26 21:53:35 -0500

    initial commit

---
+++ middleware/logEvents.js
@@ -0,0 +1,31 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (message, logName) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+  console.log(logItem);
+  const logPath = path.join(__dirname, "..", "logs");
+  try {
+    if (!fs.existsSync(logPath)) {
+      await fsPromises.mkdir(logPath);
+    }
+
+    await fsPromises.appendFile(path.join(logPath, logName), logItem);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// custom middleware logger
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, "reqLog.txt");
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
+module.exports = { logger, logEvents };
