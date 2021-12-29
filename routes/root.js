644a72ac68f4f704e34c8c88004a6d8a34385092
scottdiemer <sdiemer@cfl.rr.com>
2021-12-28 20:43:35 -0500

    setup controller for employees and deleted no longer needed routes

--- routes/root.js
+++ routes/root.js
@@ -8,12 +8,4 @@ router.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(viewsPath, "index.html"));
 });
 
-router.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(viewsPath, "new-page.html"));
-});
-
-router.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html"); // 302 by default
-});
-
-module.exports = router
+module.exports = router;
